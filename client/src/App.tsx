6e16c7277b4350082600b31f833968e13850e55a
louie <rubinlouie41@gmail.com>
2026-04-21 10:20:48 +0800

    freshly installed react vite ts  with tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+       <h1 className='text-4xl font-bold text-blue-900'>Get started</h1>
+      
+      </section>
+    </>
+  )
+}
+
+export default App
